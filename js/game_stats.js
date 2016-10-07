b1b8aa1c1dd38ac24a03abc7c7d6c4911fb182eb
torsten92 <torstengustafsson1992@gmail.com>
2016-10-07 21:00:27 +0200

    deleted a duplicate file

--- js/game_stats.js
+++
@@ -1,5 +0,0 @@
-
-var level = 1; // current unlocked level
-var engine_level = 0; // current powerup level of engines
-var weapon_level = 0; // current powerup level of weapons
-var shield_level = 0; // current powerup level of shields
